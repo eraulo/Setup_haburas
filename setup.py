369aba65d4e1cc55dd6025608e0305d5fba609fc
Mariano de Deus <dedeus_mariano@yahoo.com>
2018-05-20 09:38:27 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
+import os
 from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 
 setup(
